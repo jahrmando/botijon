03f83a8651ded9979390e01da49fd9ef8899ea42
Joel <joelconty@gmail.com>
2012-05-09 00:13:29 -0700

    some tests

--- testcommand.php
+++ testcommand.php
@@ -1,5 +1,22 @@
 <?php
-include_once("include/command.php");
-include_once("commands/google.php");
-$google = new google();
-$google->process("edecan ife");
+set_time_limit(0);
+ini_set('display_errors', 'on');
+try {
+	$systemroot = dirname(__FILE__);
+	if ( ! chdir($systemroot)){
+		throw new Exception('No se pudo cambiar de directorio a ' . $systemroot);
+	}
+	if ( ! include_once ("init.php")){
+		throw new Exception('No se pudo encontrar el archivo de inicialización.');
+	}
+	
+	include_once("include/command.php");
+	include_once("commands/seen.php");
+	$seen = new seen();
+	$seen->process('chapulin');
+	print_r($seen);
+	
+} catch (Exception $e){
+	print "Ocurrió un error durante la ejecucion: " . $e->getMessage();
+}
+echo "\n";
