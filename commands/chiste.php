26d7edda2b8dce737563c7c1682245690e6b8cac
Joel <joelconty@gmail.com>
2013-03-23 00:26:47 -0700

    fix chiste command

--- commands/chiste.php
+++ commands/chiste.php
@@ -13,11 +13,36 @@ class chiste extends command {
 	public function process($args){
 		$this->output = "";
 		
-		$chiste = file_get_contents("http://www.chistes.com/ChisteAlAzar.asp");
-		$chiste = str_replace("\n", "", $chiste);
-		$chiste = str_replace("\r", "", $chiste);
+		try {
+			
+			$referer = 'http://www.google.com/q=chistes';
+			$url="http://www.chistes.com/ChisteAlAzar.asp?n=3";
+			$agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+						
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			//curl_setopt($ch, CURLOPT_VERBOSE, true);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+			curl_setopt($ch, CURLOPT_REFERER, $referer);
+			curl_setopt($ch, CURLOPT_URL,$url);
+			$chiste = curl_exec($ch);
+			curl_close($ch);
+			
+			if (empty($chiste)){
+				$this->reply('No pude obtener un chiste para contarles... Lo siento.');
+				return;
+			} 		
+				
+			$chiste = str_replace("\n", "", $chiste);
+			$chiste = str_replace("\r", "", $chiste);
 
-		
+		} catch ( Exception $e){
+			print $e->getMessage();
+			$this->reply('No pude obtener un chiste para contarles... Lo siento.');
+			return;
+		}
+
 		try{
 			preg_match('/<div class="chiste">(.*?)<\/div>/', $chiste, $matches);
 			$elchiste = $matches[1];
