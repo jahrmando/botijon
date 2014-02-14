b0d018a3e01713fd0650cfc3d5d31b6e1f832f6f
Roberto Carlos Garcia Luis <rkgarcia@zonainter.org>
2014-02-14 13:58:16 -0600

    Agregando comando dicc que busca en la web de la RAE

---
+++ commands/dicc.php
@@ -0,0 +1,41 @@
+<?php
+class dicc extends command {
+
+	public function __construct(){
+		$this->name = 'dicc';
+		$this->public = true;
+	}
+	
+	public function help(){
+		return "Uso: !definicion <palabra> . Devuelve el significado de la palabra según la RAE.";
+	}
+	
+	public function process($args){
+		$args = trim($args);
+		$cArgs = count(split(" ",$args));
+		$this->output = "";
+		if( $cArgs>1 ){
+			$this->output = "Debe ser una palabra no una frase";
+		}else{
+			if( !strlen($args)>0 ){
+				$this->output = "Cadena demasiado corta.";
+			}else{
+				$url = "http://lema.rae.es/drae/srv/search?val=".urlencode($args);
+				$respuesta = file_get_contents($url);
+				$datos = "";
+				if( preg_match('/<div[^<>]*[^<>]*>(?<content>.*?)<\/div>/', $respuesta , $datos )){
+					$resultados = array();
+					preg_match_all('/<p[^<>]*class="q"[^<>]*>(?<content>.*?)<\/p>/', @$datos["content"] , $resultados );
+					$resultados = array_slice($resultados["content"],0,4);
+					$output = array();
+					foreach( $resultados as $key => $resultado ){
+						array_push($output, html_entity_decode(trim(strip_tags($resultado))));
+					}
+					$this->output = join("\n", $output);
+				}else{
+					$this->output = "No obtuvimos resultados para tu palabra.";
+				}
+			}
+		}
+	}
+}
